a2434829187783d4d240ba6b69e8e6d577bd5e0a
252ta04079-dot <252ta04079@ashokacollege.in>
2026-09-15 15:49:54 +0530

    Create Population.py
    This Python program is used to calculate the remaining population after a certain number of people are reduced. It takes the current population and reduction value as input and displays the remaining population.

---
+++ Population.py
@@ -0,0 +1,8 @@
+population = int(input("Enter the current population: "))
+reduction = int(input("Enter the number of people to reduce: "))
+
+remaining_population = population - reduction
+
+print("Current Population:", population)
+print("Reduced Population:", reduction)
+print("Remaining Population:", remaining_population)
